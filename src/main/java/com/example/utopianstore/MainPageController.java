75b75a923bb1eec3808465041a321157b1f57a42
Himanshu Satone <s.himanshu1707@gmail.com>
2023-01-12 12:18:09 +0530

    Implimentaion of Logout Button functionality

--- src/main/java/com/example/utopianstore/MainPageController.java
+++ src/main/java/com/example/utopianstore/MainPageController.java
@@ -6,8 +6,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -21,15 +24,34 @@ public class MainPageController {
     @FXML
     Label nameLabel;
     @FXML
-    Button logoutButton;
+    private Button logoutButton;
+    @FXML
+    private AnchorPane scenePane;
 
     //Function to go on Login Page on click of logout button
-    public void goToLoginPage(ActionEvent event) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("LoginFile.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+    public void logout(ActionEvent event) throws IOException {
+
+//        Parent root = FXMLLoader.load(getClass().getResource("LoginFile.fxml"));
+//        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+//        scene = new Scene(root);
+//        stage.setScene(scene);
+//        stage.show();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Do you want to save before exiting?: ");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("You successfully logout");
+            //stage.close();
+            Parent root = FXMLLoader.load(getClass().getResource("LoginFile.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
     }
 
     //Function to display username on welcome bar
--- src/main/java/com/example/utopianstore/Utopian.java
+++ src/main/java/com/example/utopianstore/Utopian.java
@@ -1,9 +1,12 @@
 package com.example.utopianstore;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -26,6 +29,31 @@ public class Utopian extends Application {
 
         stage.setScene(scene);
         stage.show();
+
+        //Set stage to call logoutOnHitRedCross function on press of Red Cross Button
+        stage.setOnCloseRequest(event -> {
+            try {
+                event.consume();
+                logoutOnHitRedCross(stage);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    //Function to logout on hit of Red Cross Button
+    public void logoutOnHitRedCross(Stage stage) throws IOException {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to Exit !");
+        alert.setContentText("Do you want to save before exiting?: ");
+
+        if(alert.showAndWait().get() == ButtonType.OK){
+
+            System.out.println("You successfully logout");
+            stage.close();
+        }
     }
 
     public static void main(String[] args) {
