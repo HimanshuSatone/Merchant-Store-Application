c1ef35b52eacf85420826cd3ee94ac922b740e1c
Himanshu Satone <s.himanshu1707@gmail.com>
2023-01-13 13:19:13 +0530

    Font and color change

--- src/main/java/com/example/utopianstore/SignupController.java
+++ src/main/java/com/example/utopianstore/SignupController.java
@@ -1,10 +1,12 @@
 package com.example.utopianstore;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +17,18 @@ public class SignupController {
     private Scene scene;
     private Parent root;
 
+    @FXML
+    Button backButton;
+    //Functionality for back button
+    public void setBackButton(ActionEvent event) throws IOException {
+
+        Parent root = FXMLLoader.load(getClass().getResource("LoginFile.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 
     //Function to switch to login page on click to signup button
     public void switchToLogin(ActionEvent event) throws IOException {
